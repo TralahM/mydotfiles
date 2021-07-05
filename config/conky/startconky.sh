f812e8f3cf5cfb8765262dc4c1d1f3bbd806337f
TralahM <briantralah@gmail.com>
2021-07-05 22:51:46 +0300

    new conky configurations
    Signed-off-by: TralahM <briantralah@gmail.com>

--- config/conky/startconky.sh
+++ config/conky/startconky.sh
@@ -6,4 +6,5 @@ kill -9 $(pgrep conky)
 conky -c $CONKY_DIR/conky.conf &2>/dev/null
 conky -c $CONKY_DIR/conky_weather.conf &2>/dev/null
 conky -c $CONKY_DIR/conky_TT.conf &2>/dev/null
-conky -c $CONKY_DIR/conky_system.conf &2>/dev/null
+# conky -c $CONKY_DIR/conky_system.conf &2>/dev/null
+conky -c $CONKY_DIR/conky_crypto_currency.conf &2>/dev/null
